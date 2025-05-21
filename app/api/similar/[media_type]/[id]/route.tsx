6204664ebdf75ecee1182b2a5d5f51d7e8aa8f79
onairx <vnadipudi@outlook.com>
2025-05-21 12:17:04 +0530

    fixes

--- app/api/similar/[media_type]/[id]/route.tsx
+++ app/api/similar/[media_type]/[id]/route.tsx
@@ -1,19 +1,22 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { media_type: string; id: string } }) {
-    const apiKey = process.env.NEXT_PUBLIC_API_TMDBE
-    const { media_type, id } = params
-    const url = `https://api.themoviedb.org/3/${media_type}/${id}/recommendations?page=1`
+export async function GET(request: NextRequest) {
+    const apiKey = process.env.NEXT_PUBLIC_API_TMDBE;
+    const { pathname } = request.nextUrl;
+    const pathParts = pathname.split("/");
+    const media_type = pathParts[pathParts.length - 2];
+    const id = pathParts[pathParts.length - 1];
+    const url = `https://api.themoviedb.org/3/${media_type}/${id}/recommendations?page=1`;
     try {
         const response = await fetch(url, {
             headers: {
                 Authorization: `Bearer ${apiKey}`
             }
-        })
-        const data = await response.json()
-        return NextResponse.json(data)
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ results: [] })
+        });
+        const data = await response.json();
+        return NextResponse.json(data);
+    } catch {
+        return NextResponse.json({ results: [] });
     }
 }
